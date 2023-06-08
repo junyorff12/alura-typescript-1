629ee5753926201a796de335ffc79445cc3e9eb5
junyorff12 <junyorff12@gmail.com>
2023-06-08 10:31:49 -0300

    instanciado negociacao controller

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,4 +1,7 @@
-import { Negociacao } from "./models/negociacao";
-const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao.data);
-console.log(negociacao.volume);
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
+const negociacao = new NegociacaoController;
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    negociacao.adiciona();
+});
